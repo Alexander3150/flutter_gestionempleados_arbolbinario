e347b1806b9b9ee2d0d79d13f1077f126f13cd0c
Melbyn <mxutucb@miumg.edu.gt>
2025-03-19 00:09:29 -0600

    Commit Creacion de metodos para los 3 recorridos

--- lib/models/ArbolBinario.dart
+++ lib/models/ArbolBinario.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_gestionempleados_arbolbinario/models/Empleado.dart';
 import 'package:flutter_gestionempleados_arbolbinario/models/Nodo.dart';
 
 class Arbolbinario {
@@ -38,6 +39,73 @@ class Arbolbinario {
     } else {
       // Caso contrario se busca en el subarbol derecho
       return _buscarNodo(nodo.derecho, id);
+    }
+  }
+
+  // Método para insertar un empleado en el árbol
+  bool insertar(int id, String nombre) {
+    if (_buscarNodo(raiz, id) != null) {
+      // ID ya existe, no se inserta
+      return false;
+    }
+
+    Empleado nuevoEmpleado = Empleado(id, nombre);
+    Nodo nuevoNodo = Nodo(nuevoEmpleado);
+
+    if (raiz == null) {
+      raiz = nuevoNodo;
+    } else {
+      _insertarNodo(raiz, nuevoNodo);
+    }
+    return true; //Empleado guardado con exito
+  }
+
+  // Crear una lista para almacenar el recorido
+  String preorden() {
+    List<String> resultado = [];
+    _preorden(raiz, resultado);
+    return resultado.join("  -> ");
+  }
+
+  // Recorido preorden Raiz- Izquierda-Derecha
+  void _preorden(Nodo? nodo, List<String> resultado) {
+    if (nodo != null) {
+      resultado.add('${nodo.empleado.id}: ${nodo.empleado.nombre}');
+      // Jalar los datos de la izquierda y derecha
+      _preorden(nodo.izquierdo, resultado);
+      _preorden(nodo.derecho, resultado);
+    }
+  }
+
+  // Crear una lista para almacenar el recorido
+  String inorden() {
+    List<String> resultado = [];
+    _inorden(raiz, resultado);
+    return resultado.join("  -> ");
+  }
+
+  // Recorido inorder izquierda - raiz - derecha
+  void _inorden(Nodo? nodo, List<String> resultado) {
+    if (nodo != null) {
+      _inorden(nodo.izquierdo, resultado);
+      resultado.add('${nodo.empleado.id}: ${nodo.empleado.nombre}');
+      _inorden(nodo.derecho, resultado);
+    }
+  }
+
+  // Crear una lista para almacenar el recorido
+  String postorden() {
+    List<String> resultado = [];
+    _postorden(raiz, resultado);
+    return resultado.join("  -> ");
+  }
+
+  // Recorido postorden Izquierda - Derecha- Raiz
+  void _postorden(Nodo? nodo, resultado) {
+    if (nodo != null) {
+      _postorden(nodo.izquierdo, resultado);
+      _postorden(nodo.derecho, resultado);
+      resultado.add('${nodo.empleado.id}: ${nodo.empleado.nombre}');
     }
   }
 }
